a870b8fb330c0962924ff0d7cd66b4f45416c985
inventive-technology <inventive.technology@outlook.com>
2026-01-30 21:36:31 +0000

    Added trade stream support

---
+++ src/main/java/com/binance/api/client/domain/market/Trade.java
@@ -0,0 +1,91 @@
+package com.binance.api.client.domain.market;
+
+import com.binance.api.client.constant.BinanceApiConstants;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+/**
+ * A trade event for a symbol.
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Trade {
+
+  @JsonProperty("t")
+  private long tradeId;
+
+  @JsonProperty("p")
+  private String price;
+
+  @JsonProperty("q")
+  private String quantity;
+
+  @JsonProperty("T")
+  private long tradeTime;
+
+  @JsonProperty("m")
+  private boolean isBuyerMaker;
+
+  @JsonProperty("M")
+  private boolean ignore;
+
+  public long getTradeId() {
+    return tradeId;
+  }
+
+  public void setTradeId(long tradeId) {
+    this.tradeId = tradeId;
+  }
+
+  public String getPrice() {
+    return price;
+  }
+
+  public void setPrice(String price) {
+    this.price = price;
+  }
+
+  public String getQuantity() {
+    return quantity;
+  }
+
+  public void setQuantity(String quantity) {
+    this.quantity = quantity;
+  }
+
+  public long getTradeTime() {
+    return tradeTime;
+  }
+
+  public void setTradeTime(long tradeTime) {
+    this.tradeTime = tradeTime;
+  }
+
+  public boolean isBuyerMaker() {
+    return isBuyerMaker;
+  }
+
+  public void setBuyerMaker(boolean buyerMaker) {
+    isBuyerMaker = buyerMaker;
+  }
+
+  public boolean isIgnore() {
+    return ignore;
+  }
+
+  public void setIgnore(boolean ignore) {
+    this.ignore = ignore;
+  }
+
+  @Override
+  public String toString() {
+    return new ToStringBuilder(this, BinanceApiConstants.TO_STRING_BUILDER_STYLE)
+        .append("tradeId", tradeId)
+        .append("price", price)
+        .append("quantity", quantity)
+        .append("tradeTime", tradeTime)
+        .append("isBuyerMaker", isBuyerMaker)
+        .append("ignore", ignore)
+        .toString();
+  }
+}
